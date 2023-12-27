61bdb58fccddaaccc8322ac0d8e8b7cbbea477d8
Elecrow <24800915+Elecrow-RD@users.noreply.github.com>
2023-12-27 18:05:55 +0800

    Add files via upload

---
+++ Examples/sound.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# http://elecrow.com/
+
+import RPi.GPIO as GPIO
+import time
+
+# define sound pin
+sound_pin = 24
+# set GPIO mode to GPIO.BOARD
+GPIO.setmode(GPIO.BCM)
+# setup pin as INPUT
+GPIO.setup(sound_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        # check if sound detected or not
+        if(GPIO.input(sound_pin)==GPIO.LOW):
+            print('Sound Detected')
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    # CTRL+C detected, cleaning and quitting the script
+    GPIO.cleanup()
